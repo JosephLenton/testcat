c3eef8370cbd77a524f5591df6333d64a9dc0bb7
josephlenton@gmail.com <josephlenton@gmail.com>
2022-01-14 20:59:38 +0000

    refactor: remove multiple string duplication from description_to_identifier

--- src/common/grammar.rs
+++ src/common/grammar.rs
@@ -37,25 +37,95 @@ where
     }
 }
 
-fn description_to_identifier(mut description: String) -> String {
-    if description.starts_with('"') {
-        description = description.as_str()[1..description.len() - 1].to_string();
-    }
+fn description_to_identifier(description: String) -> String {
+    let mut chars = description.chars().into_iter();
+    let mut dest = String::with_capacity(description.len());
+    let mut is_at_start = true;
 
-    for i in 0..description.len() {
-        if description.get(i..i + 1) == Some(" ") {
-            description.replace_range(i..i + 1, "_");
+    while let Some(c) = chars.next() {
+      // Find start of the description.
+      if is_at_start {
+        if c.is_alphanumeric() || c == '_' {
+          if !c.is_alphabetic() && c != '_' {
+            dest.push('_');
+          }
+
+          dest.push(c);
+          is_at_start = false;
         }
+
+        continue;
+      }
+
+      if c.is_alphanumeric() || c == '_' {
+        dest.push(c);
+        continue;
+      }
+
+      if c.is_whitespace() {
+        dest.push('_');
+        continue;
+      }
     }
 
-    if description.len() == 0 {
-        return description;
-    }
+    dest
+}
 
-    let first_char = description.chars().next().unwrap();
-    if !first_char.is_alphabetic() && first_char != '_' {
-        description = format!("_{description}");
-    }
+#[cfg(test)]
+mod description_to_identifier {
+  use super::*;
 
-    description
+  #[test]
+  fn it_should_return_an_empty_string_if_given_string_is_empty() {
+    let result = description_to_identifier("".to_string());
+    assert_eq!(result, "");
+  }
+
+  #[test]
+  fn it_should_return_same_identifier_if_all_valid() {
+    let result = description_to_identifier("foo".to_string());
+    assert_eq!(result, "foo");
+  }
+
+  #[test]
+  fn it_should_prefix_with_an_underscore_if_begins_with_numbers() {
+    let result = description_to_identifier("123".to_string());
+    assert_eq!(result, "_123");
+  }
+
+  #[test]
+  fn it_should_turn_spaces_into_underscores() {
+    let result = description_to_identifier("foo bar".to_string());
+    assert_eq!(result, "foo_bar");
+  }
+
+  #[test]
+  fn it_should_trim_a_starting_quote() {
+    let result = description_to_identifier("\"foo".to_string());
+    assert_eq!(result, "foo");
+  }
+
+  #[test]
+  fn it_should_trim_an_ending_quote() {
+    let result = description_to_identifier("foo\"".to_string());
+    assert_eq!(result, "foo");
+  }
+
+  #[test]
+  fn it_should_trim_start_and_ending_quotes() {
+    let result = description_to_identifier("\"foo\"".to_string());
+    assert_eq!(result, "foo");
+  }
+
+  #[test]
+  fn it_should_trim_starting_whitespace() {
+    let result = description_to_identifier("  foo".to_string());
+    assert_eq!(result, "foo");
+  }
+
+  #[test]
+  fn it_should_drop_non_alphanumeric_characters() {
+    let result = description_to_identifier("doesn't".to_string());
+    assert_eq!(result, "doesnt");
+  }
 }
